42fb61a4e6e9a99e2105fbb689fd05ede9fc5741
Tamanna-G <tamanna.gulati.625@gmail.com>
2025-07-07 11:22:37 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //IIFE to wrap pokemonList
-let pokemonRepository = (function()) {
+let pokemonRepository = (function() {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   
